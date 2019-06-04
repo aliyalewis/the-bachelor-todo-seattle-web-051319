063f219589343b7a5719bb702903098590cf4295
github username <github email address>
2019-06-04 13:39:29 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,53 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants|
+    if contestants["status"].downcase == "winner"
+      return contestants["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant_info|
+      if contestant_info["occupation"] == occupation
+        return contestant_info["name"]
+    # binding.pry
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        return contestant_info["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum_of_ages = 0
+  num_of_contestants = 0
+  data[season].each do |contestants|
+    sum_of_ages += contestants["age"].to_i
+    num_of_contestants += 1
+    # binding.pry
+  end
+  (sum_of_ages.to_f/num_of_contestants).round
 end
