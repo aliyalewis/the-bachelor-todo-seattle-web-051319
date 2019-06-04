387b124f2b9dae63376486c8e6cacf9f0ef8f54d
github username <github email address>
2019-06-04 13:44:14 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,6 +8,7 @@ def get_first_name_of_season_winner(data, season)
   end
 end
 
+
 def get_contestant_name(data, occupation)
   data.each do |season, contestants|
     contestants.each do |contestant_info|
